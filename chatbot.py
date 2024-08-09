1af83400ffff2abb1d64139807fb4dc98e1e0dc7
Sarad Mishra <62197447+sharad28@users.noreply.github.com>
2024-08-09 09:58:59 +0000

    chat and model details

--- chatbot.py
+++ chatbot.py
@@ -10,3 +10,29 @@ for message in st.session_state.chat_history:
     with st.chat_history(message['role']):
         st.markdown(message['text'])
 
+bedrockClinet = boto3.client('bedrock-agent-runtime','us-east-1')
+
+def getAnswers(questions):
+    knowledgeBaseResponse = bedrockClinet.retrieve_and_generate(
+        input={'text':questions},
+        retrieveAndGenerateConfiguration ={
+            'knowledgeBaseConfiguration':{
+                'knowledgeBaseId':'EOTHLX655R'
+                'modelArn':'arn:aws:bedrock:us-east-1::foundation-model/cohere.command-text-v14'
+            },
+            'type':'KNOWLEDGE_BASE'
+        })
+    return knowledgeBaseResponse
+
+questions = st.chat_input('Enter your question here..')
+
+if questions :
+    with st.chat_message('user'):
+        st.markdown(questions)
+    st.session_state.chat_history.append({"role":'user','text':questions})
+
+    response = getAnswers(questions)
+
+    answer = response['output']['text']
+
+    
