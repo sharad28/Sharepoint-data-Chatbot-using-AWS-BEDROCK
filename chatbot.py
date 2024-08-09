ee99a6a6b9818a392223e125d06b23581a533c3d
Sarad Mishra <62197447+sharad28@users.noreply.github.com>
2024-08-09 09:23:04 +0000

    first draft

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,12 @@
-pass
+import boto3
+import streamlit as st
+
+st.subheader('Chatbot with SharePoint Custom Data',divider='rainbow')
+
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history=[]
+
+for message in st.session_state.chat_history:
+    with st.chat_history(message['role']):
+        st.markdown(message['text'])
+
